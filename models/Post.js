75d10748c46cf55860fa40d4804e695d9fb942e7
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-08-17 08:56:45 -0700

    added a new database to fix error 1100 for mongodb

--- models/Post.js
+++ models/Post.js
@@ -6,7 +6,7 @@ const CommentSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId, ref: 'User',
         sparse: true,
-        unique: false
+        unique: false,
     },
 
     text: {
