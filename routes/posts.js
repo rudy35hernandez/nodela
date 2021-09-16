91c894a8fccb094d4fc6712d2d89b7b6683c3f0b
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-09-16 10:28:53 -0700

    added a change to the login process, changed type from text to password

--- routes/posts.js
+++ routes/posts.js
@@ -6,12 +6,12 @@ const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 
 
-router.post('/createPost', postsController.createPost)
+router.post('/createPost', postsController.createPost) /// creates a new post or change to the site
 
-router.get('/:id', ensureAuth, postsController.getPost)
+router.get('/:id', ensureAuth, postsController.getPost) /// this requests the data you're asking for
 // router.put('/likePost/:id', postsController.likePost)
-router.put('/addComment/:id', ensureAuth, postsController.addComment)
-router.delete('/deleteComment', ensureAuth, postsController.deleteComment)
+router.put('/addComment/:id', ensureAuth, postsController.addComment) /// updates your post
+router.delete('/deleteComment', ensureAuth, postsController.deleteComment) //// deletes something
 router.delete('/deletePost', postsController.deletePost)
 
 
